ae0a1d5e110022596beb2c9b7b9e849a3e8eb8be
Atharv-bali <160755403+Atharv-bali@users.noreply.github.com>
2026-04-28 22:13:40 +0530

    Delete geu-connect/server directory

--- geu-connect/server/routes/interviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { scheduleInterview, getUserInterviews } = require('../controllers/interviewController');
-const authMiddleware = require('../middleware/auth');
-
-router.post('/', authMiddleware, scheduleInterview);
-router.get('/', authMiddleware, getUserInterviews);
-
-module.exports = router;
